b55aa43b212ef18a294425a2840f97f01cae51fc
KYS <yungsang1111@gmail.com>
2022-01-18 21:02:04 +0900

    Fix try-catch to if-else in test_script.js

--- test_script.js
+++ test_script.js
@@ -17,7 +17,7 @@ function inputCountryName() {
     const EncodingKey = 'DbakMaVDHqAIbWiwMG9Hm67W398h5mBPRRUwiqQALm0frxh4fCLVEvgeevHaSz8OjHFQ6AtSyaofLHpwbsnQsA%3D%3D';
     const DecodingKey = 'DbakMaVDHqAIbWiwMG9Hm67W398h5mBPRRUwiqQALm0frxh4fCLVEvgeevHaSz8OjHFQ6AtSyaofLHpwbsnQsA==';
     var url = 'http://apis.data.go.kr/1262000/CountryOverseasArrivalsService/getCountryOverseasArrivalsList'; /*URL*/
-    var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + DecodingKey; /*Service Key*/
+    var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + EncodingKey; /*Service Key*/
     queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('JSON'); /**/
     queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /**/
     queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
@@ -29,15 +29,18 @@ function inputCountryName() {
         if (this.readyState == 4) {
             const JSONobj = JSON.parse(this.response); // JSON 객체 받기
             const JSONdata = JSONobj['data']; // JSON 객체중 data 부분 받기
-            let countrydata;
+            let countrydata; // 웹에 출력할 데이터
+
             for (i in JSONdata) { // data를 순회하면서 html_origin_cn 받기
                 countrydata = JSONdata[i]['html_origin_cn'];
             }
-            try { // 나라 이름과 ,iso 코드가 제대로 입력되었을 경우
+
+            if(countrydata != undefined){ // 나라이름과 iso 코드가 제대로 입력된 경우
                 //countrydata = countrydata.replace(/(<([^>]+)>)/ig, "");
                 title.innerHTML = '<hr>' + countryName + '(' + isoCode + ')' + ' 해외 입국자 조치사항';
                 showInfo.innerHTML = countrydata;
-            } catch (error) { // 아닌경우 오류처리
+            }
+            else{ // 아닌경우 오류 알림
                 alert("나라이름과 iso코드를 정확히 입력해주세요");
                 title.innerHTML = '';
                 showInfo.innerHTML = '';
