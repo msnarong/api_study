1627945d497e0e05afeb1a792a2df1fd7c0b6bca
KYS <yungsang1111@gmail.com>
2022-01-20 15:52:43 +0900

    Add try catch

--- test_script.js
+++ test_script.js
@@ -20,31 +20,36 @@ function inputCountryName() {
     var queryParams = '?' + encodeURIComponent('serviceKey') + '=' + EncodingKey; /*Service Key*/
     queryParams += '&' + encodeURIComponent('returnType') + '=' + encodeURIComponent('JSON'); /**/
     queryParams += '&' + encodeURIComponent('numOfRows') + '=' + encodeURIComponent('10'); /**/
-    queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('1'); /**/
+    queryParams += '&' + encodeURIComponent('pageNo') + '=' + encodeURIComponent('3'); /**/
     queryParams += '&' + encodeURIComponent('cond[country_nm::EQ]') + '=' + encodeURIComponent(countryName); /**/
     queryParams += '&' + encodeURIComponent('cond[country_iso_alp2::EQ]') + '=' + encodeURIComponent(isoCode); /**/
 
     xhr.open('GET', url + queryParams);
     xhr.onreadystatechange = function () {
         if (this.readyState == 4) {
-            const JSONobj = JSON.parse(this.response); // JSON 객체 받기
-            const JSONdata = JSONobj['data']; // JSON 객체중 data 부분 받기
-            let countrydata; // 웹에 출력할 데이터
+            try{
+                const JSONobj = JSON.parse(this.response);
+                const JSONdata = JSONobj['data']; // JSON 객체중 data 부분 받기
+                let countrydata; // 웹에 출력할 데이터
 
-            for (i in JSONdata) { // data를 순회하면서 html_origin_cn 받기
-                countrydata = JSONdata[i]['html_origin_cn'];
-            }
+                for (i in JSONdata) { // data를 순회하면서 html_origin_cn 받기
+                    countrydata = JSONdata[i]['html_origin_cn'];
+                }
 
-            if(countrydata != undefined){ // 나라이름과 iso 코드가 제대로 입력된 경우
-                //countrydata = countrydata.replace(/(<([^>]+)>)/ig, "");
-                title.innerHTML = '<hr>' + countryName + '(' + isoCode + ')' + ' 해외 입국자 조치사항';
-                showInfo.innerHTML = countrydata;
-            }
-            else{ // 아닌경우 오류 알림
-                alert("나라이름과 iso코드를 정확히 입력해주세요");
-                title.innerHTML = '';
-                showInfo.innerHTML = '';
-            }
+                if (countrydata != undefined) { // 나라이름과 iso 코드가 제대로 입력된 경우
+                    //countrydata = countrydata.replace(/(<([^>]+)>)/ig, "");
+                    title.innerHTML = '<hr>' + countryName + '(' + isoCode + ')' + ' 해외 입국자 조치사항';
+                    showInfo.innerHTML = countrydata;
+                }
+                else { // 아닌경우 오류 알림
+                    alert("나라이름과 iso코드를 정확히 입력해주세요");
+                    title.innerHTML = '';
+                    showInfo.innerHTML = '';   
+                }
+            }    
+            catch(error){
+                alert("연결안됨 시발");
+            }        
         }
     };
     xhr.send('');
